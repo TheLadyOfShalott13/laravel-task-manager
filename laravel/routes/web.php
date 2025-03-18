3eb5047ac819926273ef37d8c00f15a1a4cd9179
TheLadyOfShalott13 <angel.locsin.fan95@gmail.com>
2025-03-18 22:42:54 +0400

    cleaning up web routes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
-use App\Http\Controllers\Auth\ApiAuthController;
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::resource('tasks', TaskController::class)->middleware('auth');
-
 Auth::routes();
-
-Route::post('/login', [ApiAuthController::class, 'login']);
+Route::get('/', function () { return view('welcome'); });
+Route::resource('tasks', TaskController::class)->middleware('auth');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
