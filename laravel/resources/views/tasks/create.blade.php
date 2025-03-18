3c2ede98626406fc73e120245b298114d4791c97
TheLadyOfShalott13 <angel.locsin.fan95@gmail.com>
2025-03-18 23:11:47 +0400

    Refined create page template

--- laravel/resources/views/tasks/create.blade.php
+++ laravel/resources/views/tasks/create.blade.php
@@ -8,21 +8,66 @@
     <form method="POST" action="{{ route('tasks.store') }}">
         @csrf
         <div class="mb-3">
-            <label for="title" class="form-label">Title</label>
-            <input type="text" id="title" name="title" class="form-control" required value="{{ old('title') }}">
+            <label
+                for="title"
+                class="form-label"
+            >
+                Task title
+            </label>
+            <input
+                type="text"
+                id="title"
+                name="title"
+                class="form-control"
+                maxlength="100"
+                required
+            >
         </div>
 
         <div class="mb-3">
-            <label for="description" class="form-label">Description</label>
-            <textarea id="description" name="description" class="form-control" rows="3">{{ old('description') }}</textarea>
+            <label
+                for="description"
+                class="form-label"
+            >
+                Task Description
+            </label>
+            <textarea
+                id="description"
+                name="description"
+                class="form-control"
+                rows="3"
+            >
+            </textarea>
         </div>
 
         <div class="mb-3">
-            <label for="due_date" class="form-label">Due Date</label>
-            <input type="date" id="due_date" name="due_date" class="form-control" value="{{ old('due_date') }}">
+            <label
+                for="due_date"
+                class="form-label"
+            >
+                Task Due Date
+            </label>
+            <input
+                type="date"
+                id="due_date"
+                name="due_date"
+                class="form-control"
+                required
+                value="{{ old('due_date') }}"
+            >
         </div>
 
-        <button type="submit" class="btn btn-primary">Create Task</button>
-        <a href="{{ route('tasks.index') }}" class="btn btn-secondary">Cancel</a>
+        <button
+            type="submit"
+            class="btn btn-primary"
+        >
+            Create Task
+        </button>
+        <a
+            href="{{ route('tasks.index') }}"
+            class="btn btn-secondary"
+        >
+            Cancel
+        </a>
     </form>
 @endsection
