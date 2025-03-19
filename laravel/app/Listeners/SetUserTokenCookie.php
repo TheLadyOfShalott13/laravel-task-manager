e978a12ac495363c18df90422de109ce1e571e7d
TheLadyOfShalott13 <angel.locsin.fan95@gmail.com>
2025-03-19 07:59:31 +0400

    Bug fix for cookie setting value

--- laravel/app/Listeners/SetUserTokenCookie.php
+++ laravel/app/Listeners/SetUserTokenCookie.php
@@ -20,22 +20,21 @@ class SetUserTokenCookie
 
     /**
      * @param Authenticated $event
-     * @return void
      * Function to fire after the Authentication of a user
      * Sets the token for site wide user API usage
      */
-    public function handle(Authenticated $event): void
+    public function handle(Authenticated $event)
     {
         $token_name             = config('constants.USER_API_TOKEN_COOKIE');
         $logged_in_user         = $event->user;
-        $authorization_token    = $logged_in_user->createToken($token_name)->accessToken;
+        $authorization_token    = $logged_in_user->createToken($token_name)->plainTextToken;
         $cookie                 = Cookie::make(
                                     $token_name,
                                     $authorization_token,
                                     60 * 24 * 365,  //put a large value ie 1 year just to keep flexibility in the session of the user
                                     '/',
                                     null,
-                                    true,
+                                    false,
                                     false,
                                     'strict'
                                 );
